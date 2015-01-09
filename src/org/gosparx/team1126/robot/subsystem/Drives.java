35edfd3613fb946fbdb67d7c3b1962aab741b762
Michael-Edwards777 <Mikee777@rochester.rr.com>
2015-01-09 18:55:33 -0500

    Declared and Instantiated victors, solenoids, and encoders.

--- src/org/gosparx/team1126/robot/subsystem/Drives.java
+++ src/org/gosparx/team1126/robot/subsystem/Drives.java
@@ -1,9 +1,21 @@
 package org.gosparx.team1126.robot.subsystem;
 
+import org.gosparx.team1126.robot.IO;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Victor;
+
 public class Drives extends GenericSubsystem{
 
 	private static Drives drives;
-	
+	private Victor v1LeftFront;
+	private Victor v2LeftBack;
+	private Victor v3RightFront;
+	private Victor v4RightBack;
+	private Encoder encodeLeft;
+	private Encoder encodeRight;
+	private Solenoid sol;
 	public static Drives getInstance(){
 		if(drives == null){
 			drives = new Drives("Drives", Thread.NORM_PRIORITY);
@@ -16,8 +28,15 @@ public class Drives extends GenericSubsystem{
 	}
 
 	@Override
-	protected boolean init() {	
-		return false;
+	protected boolean init() {
+		v1LeftFront = new Victor(IO.leftFrontDrives);
+		v2LeftBack = new Victor(IO.leftBackDrives);
+		v3RightFront = new Victor(IO.rightFrontDrives);
+		v4RightBack = new Victor(IO.rightBackDrives);
+		encodeLeft = new Encoder(IO.leftDrivesEncoderA, IO.leftDrivesEncoderB);
+		encodeRight = new Encoder(IO.rightDrivesEncoderA, IO.rightDrivesEncoderB);
+		sol = new Solenoid(IO.shiftingPnu);
+		return true;
 	}
 
 	@Override

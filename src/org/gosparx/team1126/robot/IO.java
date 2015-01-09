84aeda494aeaf0fc24806c77303ea580c057fe62
secretspy97 <nathan.connor@gosparx.org>
2015-01-09 18:45:00 -0500

    added joystick to IO

--- src/org/gosparx/team1126/robot/IO.java
+++ src/org/gosparx/team1126/robot/IO.java
@@ -2,17 +2,22 @@ package org.gosparx.team1126.robot;
 
 public final class IO {
 	//Motor
-	private static final int rightFrontDrives 		= 0;
-	private static final int rightBackDrives 		= 0;
-	private static final int leftFrontDrives		= 0;
-	private static final int leftBackDrives 		= 0;
+	public static final int rightFrontDrives 		= 0;
+	public static final int rightBackDrives 		= 0;
+	public static final int leftFrontDrives			= 0;
+	public static final int leftBackDrives 			= 0;
 	
 	//PNU
-	private static final int shiftingPnu 			= 0;
+	public static final int shiftingPnu 			= 0;
 	
 	//Encoders
-	private static final int rightDrivesEncoderA 	= 0;
-	private static final int rightDrivesEncoderB 	= 0;
-	private static final int leftDrivesEncoderA 	= 0;
-	private static final int leftDrivesEncoderB		= 0;
+	public static final int rightDrivesEncoderA 	= 0;
+	public static final int rightDrivesEncoderB 	= 0;
+	public static final int leftDrivesEncoderA 		= 0;
+	public static final int leftDrivesEncoderB		= 0;
+	
+	//Joystick
+	public static final int driverJoyPort			= 0;
+	
+	
 }

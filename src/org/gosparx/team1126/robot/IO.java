324827c6a3155440b420ef6dbd6009295c0b335b
secretspy97 <nathan.connor@gosparx.org>
2015-01-09 18:42:22 -0500

    added IO

---
+++ src/org/gosparx/team1126/robot/IO.java
@@ -0,0 +1,18 @@
+package org.gosparx.team1126.robot;
+
+public final class IO {
+	//Motor
+	private static final int rightFrontDrives 		= 0;
+	private static final int rightBackDrives 		= 0;
+	private static final int leftFrontDrives		= 0;
+	private static final int leftBackDrives 		= 0;
+	
+	//PNU
+	private static final int shiftingPnu 			= 0;
+	
+	//Encoders
+	private static final int rightDrivesEncoderA 	= 0;
+	private static final int rightDrivesEncoderB 	= 0;
+	private static final int leftDrivesEncoderA 	= 0;
+	private static final int leftDrivesEncoderB		= 0;
+}

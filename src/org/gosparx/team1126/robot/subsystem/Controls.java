da142c943bdb6fc8d59abd794f0ef96a13a732d2
Michael-Edwards777 <Mikee777@rochester.rr.com>
2015-01-10 15:56:23 -0500

    Added comments to all the variables and methods.

--- src/org/gosparx/team1126/robot/subsystem/Controls.java
+++ src/org/gosparx/team1126/robot/subsystem/Controls.java
@@ -7,43 +7,69 @@ import edu.wpi.first.wpilibj.Joystick;
  *
  */
 public class Controls extends GenericSubsystem{
- private Joystick driverJoy;
- 
+	/**
+	 * declares a Joystick object named driverJoy
+	 */
+	private Joystick driverJoy;
+	/**
+	 * declares a Controls object named controls
+	 */
 	private static Controls controls;
+	/**
+	 * declares a Drives object named drives
+	 */
 	private Drives drives;
+	/**
+	 * if controls == null, make a new controls
+	 * @return the new controls
+	 */
 	public static synchronized Controls getInstance(){
 		if(controls == null){
 			controls = new Controls("Controls", Thread.NORM_PRIORITY);
 		}
 		return controls;
 	}
-	
+	/**
+	 * @param name the name of the control
+	 * @param priority the priority of the control
+	 * constructor for the Controls
+	 */
 	private Controls(String name, int priority) {
 		super(name, priority);
 	}
-
+	/**@return false ~ keeps looping true ~ stops loop
+	 * instantiates a Joystick and Drives
+	 */
 	@Override
 	protected boolean init() {
 		driverJoy = new Joystick(IO.DRIVER_JOYSTICK_PORT);
 		drives = Drives.getInstance(); 
 		return true;
 	}
-
+	/**@return false ~ keeping looping true ~ end loop
+	 * sets the speed of the control axis to drives
+	 */
 	@Override
 	protected boolean execute() {
 		drives.setSpeed(driverJoy.getX(), driverJoy.getY());
 		return false;
 	}
-
+	/**
+	 * The amount of time you want to sleep for after a cycle.
+	 * 
+	 * @return the number of milliseconds you want to sleep after a cycle.
+	 */
 	@Override
 	protected long sleepTime() {
 		return 20; //not sure if this is correct time needed
 	}
-
+	/**
+	 * Where all the logged info goes
+	 */
 	@Override
 	protected void writeLog() {
 		System.out.println("Enabling");
-		
+
 	}
 
 }

8b300d271d2bede38e802aa451297105c0d6633d
Kmodos <amechler1998@gmail.com>
2015-01-10 15:35:09 -0500

    COMMENTSSSS

--- src/org/gosparx/team1126/robot/sensors/UltrasonicRangeFinder.java
+++ src/org/gosparx/team1126/robot/sensors/UltrasonicRangeFinder.java
@@ -4,16 +4,28 @@ import edu.wpi.first.wpilibj.AnalogInput;
 
 /**
  * A class to interpret data from the UltrasonicRangeFinder
- * @author Alex_Fixed
+ * @author Alex
  */
 public class UltrasonicRangeFinder {
 	
+	/**
+	 * The analog input that the sensor is in.
+	 */
 	private AnalogInput sensor;
 	
+	/**
+	 * The voltage drop per inch that the target moves
+	 */
 	private double voltsPerInch;
 	
+	/**
+	 * The voltage at the set zero position
+	 */
 	private double zeroPos;
 	
+	/**
+	 * The volts per inch drop for the MaxSonar EZ1 range finder.
+	 */
 	public static final double MAX_SONAR_EZ1_VPI = 5.0/512.0; 
 	
 	/**
@@ -27,14 +39,24 @@ public class UltrasonicRangeFinder {
 		zeroPos = 0;
 	}
 	
+	/**
+	 * Sets the zero point for the sensor at the current distance
+	 */
 	public void zero(){
 		zeroPos = sensor.getVoltage();
 	}
 	
+	/**
+	 * Resets the zero point to 0
+	 */
 	public void resetZero(){
 		zeroPos = 0;
 	}
 	
+	/**
+	 * Gets the range in inches to the nearest object
+	 * @return The range in inches
+	 */
 	public double getRange(){
 		return (sensor.getVoltage() - zeroPos) / voltsPerInch;
 	}

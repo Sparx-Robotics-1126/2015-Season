d6c805c771fc31b4ee772b843b8cd66ae01a5142
Kmodos <amechler1998@gmail.com>
2015-01-10 12:53:18 -0500

    Reordered a comment to appease Justin

--- src/org/gosparx/team1126/robot/Robot.java
+++ src/org/gosparx/team1126/robot/Robot.java
@@ -4,8 +4,8 @@ import org.gosparx.team1126.robot.subsystem.GenericSubsystem;
 import edu.wpi.first.wpilibj.SampleRobot;
 
 /**
- * @author Alex
  * The entrypoint for the robot. The constructor is called once the robot is turned on.
+ * @author Alex
  */
 public class Robot extends SampleRobot {
 
@@ -13,37 +13,37 @@ public class Robot extends SampleRobot {
 	 * An array of all of the subsystems on the robot
 	 */
 	private GenericSubsystem[] subsystems;
-	
+
 	/**
 	 * Called once every time the robot is powered on
 	 */
-    public Robot() {
-        subsystems = new GenericSubsystem[]{
-        		
-        };
-        
-        for(GenericSubsystem system: subsystems){
-        	system.start();
-        }
-    }
+	public Robot() {
+		subsystems = new GenericSubsystem[]{
 
-    /**
-     *  Called one time when the robot enters autonomous
-     */
-    public void autonomous() {
+		};
 
-    }
+		for(GenericSubsystem system: subsystems){
+			system.start();
+		}
+	}
 
-    /**
-     *  Called one time when the robot enters teleop
-     */
-    public void operatorControl() {
-        
-    }
+	/**
+	 *  Called one time when the robot enters autonomous
+	 */
+	public void autonomous() {
 
-    /**
-     *  Called one time when the robot enters test
-     */
-    public void test() {
-    }
+	}
+
+	/**
+	 *  Called one time when the robot enters teleop
+	 */
+	public void operatorControl() {
+
+	}
+
+	/**
+	 *  Called one time when the robot enters test
+	 */
+	public void test() {
+	}
 }

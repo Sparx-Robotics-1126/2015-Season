0e30fb7a1ad0e3930ef237d25a72a708a7d4d4dd
secretspy97 <nathan.connor@gosparx.org>
2015-01-14 18:51:14 -0500

    added comments

--- src/org/gosparx/team1126/robot/sensors/ColorSensor.java
+++ src/org/gosparx/team1126/robot/sensors/ColorSensor.java
@@ -9,6 +9,10 @@ import edu.wpi.first.wpilibj.DigitalOutput;
 
 public class ColorSensor {
 	
+	/**
+	 * State machine for Color
+	 *
+	 */
 	public enum Color {
 		UNKNOWN,
 		WHITE,
@@ -38,11 +42,15 @@ public class ColorSensor {
 	 */
 	private DigitalOutput lightLED;
 	
-	// we treat white as 80% of the addition of all of the colors and above
-	static final double WHITE_THRESHHOLD = (255 * 3) * .8; 
+	/**
+	 * we treat white as 80% of the addition of all of the colors and above
+	 */
+	static final double WHITE_THRESHOLD = (255 * 3) * .8; 
 	
-	// we treat black as 20% of the addition of all of the colors and below
-	static final double BLACK_THRESHHOLD = (255 * 3) * .2;
+	/**
+	 * we treat black as 20% of the addition of all of the colors and below
+	 */
+	static final double BLACK_THRESHOLD = (255 * 3) * .2;
 
 	public ColorSensor(int redChannel, int greenChannel, int blueChannel, int ledChannel){
 		redAnalogInput = new AnalogInput(redChannel);
@@ -53,21 +61,21 @@ public class ColorSensor {
 	}
 
 	/**
-	 * @return value of red (0 - 255)
+	 * @return value of red (0 - 255) values may be bigger than 255
 	 */
 	private int getRed(){
 		return redAnalogInput.getValue();
 	}
 	
 	/**
-	 * @return value of green (0 - 255)
+	 * @return value of green (0 - 255) values may be bigger than 255
 	 */
 	private int getGreen(){
 		return greenAnalogInput.getValue();
 	}
 	
 	/**
-	 * @return value of blue (0 - 255)
+	 * @return value of blue (0 - 255) values may be bigger than 255
 	 */
 	private int getBlue(){
 		return blueAnalogInput.getValue();
@@ -80,7 +88,9 @@ public class ColorSensor {
 		lightLED.set(on);
 	}
 	
-	//returns the color ID
+	/**
+	 * returns the color ID
+	 */
 	public Color getColor(){
 		int redValue = getRed();
 		int greenValue = getGreen();
@@ -105,6 +115,11 @@ public class ColorSensor {
 		return Color.UNKNOWN;
 	}
 	
+	/**
+	 * 
+	 * @param color - color wanted from Color.
+	 * @return the string value for the color
+	 */
 	public String colorToString(Color color){
 		switch (color){
 			case WHITE:
@@ -125,6 +140,11 @@ public class ColorSensor {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param color - color wanted to see
+	 * @return - if the color seen by the sensor is the same as the color wanted
+	 */
 	public boolean isColor(Color color) {
 		return(getColor() == color);
 	}

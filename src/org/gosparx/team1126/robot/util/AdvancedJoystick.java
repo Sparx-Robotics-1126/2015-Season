c1cac19774f774f78523d8283f15300ac4396f83
Kmodos <amechler1998@gmail.com>
2015-01-14 19:55:59 -0500

    Iteration 1

---
+++ src/org/gosparx/team1126/robot/util/AdvancedJoystick.java
@@ -0,0 +1,124 @@
+package org.gosparx.team1126.robot.util;
+
+import java.util.HashMap;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * A class for more advanced data collection from the joysticks.
+ * @author Alex Mechler
+ */
+public class AdvancedJoystick{
+
+	/**
+	 * The Joystick to get the data from
+	 */
+	private Joystick stick;
+
+	/**
+	 * The number of buttons on the joystick
+	 */
+	private int numButtons;
+
+	/**
+	 * The number of Axis on the joystick
+	 */
+	private int numAxis;
+
+	/**
+	 * A map of all of the previous button values, used for rising/falling edge detection
+	 */
+	private HashMap<Integer, Boolean> prevButton;
+
+	/**
+	 * A map of all of the current button values, used for rising/falling edge detection
+	 */
+	private HashMap<Integer, Boolean> currButton;
+
+	/**
+	 * A map of all of the previous axis values, currently unused
+	 */
+	private HashMap<Integer, Double> prevAxis;
+
+	/**
+	 * A map of all of the current axis values
+	 */
+	private HashMap<Integer, Double> currAxis;
+
+	/**
+	 * An instance of driverstation
+	 */
+	private DriverStation ds;
+
+	/**
+	 * Creates a new AdvancedJoystick
+	 * @param joy The Joystick to collect data from
+	 * @param numButtons The number of buttons on the Joystick
+	 * @param numAxis The number of Axis on the Joystick
+	 */
+	public AdvancedJoystick(Joystick joy, int numButtons, int numAxis){
+		stick = joy;
+		this.numButtons = numButtons;
+		this.numAxis = numAxis;
+		prevButton = new HashMap<Integer, Boolean>();
+		currButton = new HashMap<Integer, Boolean>();
+		prevAxis = new HashMap<Integer, Double>();
+		currAxis = new HashMap<Integer, Double>();
+		ds = DriverStation.getInstance();
+	}
+
+	/**
+	 * Call this before any other method using the values. Updates all of the maps
+	 */
+	public void updateValues(){
+		if(ds.isEnabled() && ds.isOperatorControl()){
+			prevButton = new HashMap<>(currButton);
+			currButton.clear();
+			for(int i = 1; i <= numButtons; i++){
+				currButton.put(i, stick.getRawButton(i));
+			}
+			prevAxis = new HashMap<>(currAxis);
+			currAxis.clear();
+			for (int i = 1; i <= numAxis; i++){
+				currAxis.put(i, stick.getRawAxis(i));
+			}
+		}
+	}
+
+	/**
+	 * returns the current value of the axis
+	 * @param id the id of the Axis
+	 * @return the current value of the axis
+	 */
+	public double getAxis(int id){
+		return currAxis.get(id);
+	}
+
+	/**
+	 * Gets the current state of the button
+	 * @param id the id of the button to collect
+	 * @return true if pressed, false if not
+	 */
+	public boolean getButton(int id){
+		return currButton.get(id);
+	}
+
+	/**
+	 * Returns if the button is on a rising edge
+	 * @param id the buttons id
+	 * @return true if it is a rising edge
+	 */
+	public boolean getRisingEdge(int id){
+		return currButton.get(id) && !prevButton.get(id);
+	}
+
+	/**
+	 * Returns if the button is on a falling edge
+	 * @param id the id of the button
+	 * @return returns if it is on a falling edge
+	 */
+	public boolean getFallingEdge(int id){
+		return !currButton.get(id) && prevButton.get(id);
+	}
+}

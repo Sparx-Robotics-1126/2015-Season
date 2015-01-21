2c818920aca9986bd0abf2f6a0a3c84cb4022d1b
Kmodos <amechler1998@gmail.com>
2015-01-21 18:20:43 -0500

    Added a boolean to inverse and comments

--- src/org/gosparx/team1126/robot/sensors/MagnetSensor.java
+++ src/org/gosparx/team1126/robot/sensors/MagnetSensor.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  * A class for interpreting the data from Magnetic Limit Switch
- * @author Alex
+ * @author Alex Mechler {amechler1998@gmail.com}
  */
 public class MagnetSensor {
 
@@ -14,25 +14,35 @@ public class MagnetSensor {
 	private DigitalInput in;
 	
 	/**
+	 * Is the output inversed
+	 */
+	private boolean inversed;
+	/**
 	 * Creates a new magnetic sensor
 	 * @param dio - The digitalinput the sensor is in
+	 * @param inverse - do we inverse the output
 	 */
-	public MagnetSensor(DigitalInput dio){
+	public MagnetSensor(DigitalInput dio, boolean inverse){
 		in = dio;
+		inversed = inverse;
 	}
-	
+
 	/**
 	 * Creates a new magnetic sensor
 	 * @param port - the port the sensor is in
+	 * @param inverse - do we inverse the output
 	 */
-	public MagnetSensor(int port){
+	public MagnetSensor(int port, boolean inverse){
 		in = new DigitalInput(port);
+		inversed = inverse;
 	}
-	
+
 	/**
 	 * @return if the sensor is tripped.
 	 */
 	public boolean isTripped(){
-		return !in.get();
+		if(inversed)
+			return !in.get();
+		return in.get();
 	}
 }
